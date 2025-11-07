5f2873b2b4cb65894365f554d660cc70b34c1648
Elhoucine Elmourabit <elhoucine.elmourabit.pro@gmail.com>
2025-11-07 12:26:51 +0100

    feat(config): Centralize all microservice configurations in the config service The config service now fetches configurations from the repository at https://github.com/ElhoucineElmourabit/ecom-config-microservices-app

--- customer-service/src/main/java/net/houcine/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/net/houcine/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package net.houcine.customerservice;
 
+import net.houcine.customerservice.config.CustomerConfigParams;
 import net.houcine.customerservice.entities.Customer;
 import net.houcine.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
